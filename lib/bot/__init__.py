139d8b2c35e8af58ca5716bd30b1cfb33ffc4f5d
BohMatej <87945844+BohMatej@users.noreply.github.com>
2021-08-31 23:49:38 +0200

    transition to hikari
    bot does ping pong thing

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,52 +1,17 @@
-from discord import Intents
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from discord.ext.commands import Bot as BotBase
+from dotenv import load_dotenv
+import os
+import hikari
 
-from ..db import db
-
-PREFIX = "+"
-OWNER_IDS = [341218733546668033]
+load_dotenv()
+bot = hikari.GatewayBot(token=os.environ.get("BOT_TOKEN"))
 
 
-class Bot(BotBase):
-	def __init__(self):
-		self.PREFIX = PREFIX
-		self.ready = False
-		self.guild = None
-		self.scheduler = AsyncIOScheduler()
+@bot.listen()
+async def on_message(event: hikari.MessageCreateEvent) -> None:
+    """Listen for messages being created."""
+    if not event.is_human:
+        # Do not respond to bots or webhooks!
+        return
 
-		super().__init__(
-			command_prefix=PREFIX, 
-			owner_ids=OWNER_IDS
-			#intents=Intents.all(),
-		)
-
-	def run(self, version):
-		self.VERSION = version
-		
-		with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
-			self.TOKEN = tf.read()
-
-		print("Running bot...")
-		super().run(self.TOKEN, reconnect=True)
-
-	async def on_connect(self):
-		print("bot online")
-
-	async def on_disconnect(self):
-		print("bot offline")
-
-	async def on_ready(self):
-		if not self.ready:
-			self.ready = True
-			self.scheduler.start()
-			print("bot ready")
-
-		else:
-			print("bot reconnected")
-
-	async def on_message(self, message):
-		pass
-
-
-bot = Bot()
+    if event.content == "!ping":
+        await event.message.respond(f"Pong! {bot.heartbeat_latency * 1_000:.0f}ms")
